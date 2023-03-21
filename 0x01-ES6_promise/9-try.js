92734c08c687ccb4fc1aebf8da0000e52e38c1ae
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-21 09:06:44 +0000

    all tasks

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
