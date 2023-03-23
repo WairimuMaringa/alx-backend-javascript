3b6dda99def9577a2888102c1e6981233170fdee
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-23 12:13:03 +0000

    three

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,22 +7,16 @@ export default class Currency {
     return this._code;
   }
   set code(value) {
-    if (value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._code = value;
   }
   get name() {
     return this._name;
   }
   set name(value) {
-    if (value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = value;
   }
 
   displayFullCurrency() {
-    return(`${this._name} (${this._code})`);
+    return `${this.name} (${this.code})`;
   }
 }
